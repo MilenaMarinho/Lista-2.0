2485eca42d0676673df4316fa56626f188796dd0
MilenaMarinho <130614123+MilenaMarinho@users.noreply.github.com>
2023-12-12 18:59:32 -0300

    Add files via upload

---
+++ Lista1Ex3/Lista1Ex3/Quadrado.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista1Ex3
+{
+    internal class Quadrado
+    {
+        private double diag;
+        private double area;
+
+        public Quadrado()
+        {
+            this.diag = 0;
+        }
+
+        public Quadrado (double diag)
+        { 
+            this.diag = diag;
+        }
+
+        public void setDiag(double diag)
+        {
+            this.diag = diag;
+        }
+
+        public double getDiag()
+        {
+            return this.diag;
+        }
+
+        public double getArea()
+        {
+            return this.area;
+        }
+
+        public void calcular()
+        {
+
+            this.area = Math.Pow(this.diag / Math.Sqrt(2), 2);
+
+        }
+
+    }
+}
---
+++ Lista1Ex6/Lista1Ex6/Quilômetros.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista1Ex6
+{
+    internal class Quilômetros
+    {
+        private double milhaM;
+        private double km;
+
+        public Quilômetros()
+        {
+            this.milhaM = 0;
+        }
+
+        public Quilômetros(double milhaM)
+        {
+            this.milhaM = milhaM;
+        }
+
+        public void setMilhaM(double milhaM)
+        {
+            this.milhaM = milhaM;
+        }
+
+        public double getMilhaM()
+        {
+            return this.milhaM;
+        }
+
+        public double getKm()
+        {
+            return this.km;
+        }
+
+        public void calcular()
+        {
+            this.km = this.milhaM * 1.852;
+        }
+    }
+}
